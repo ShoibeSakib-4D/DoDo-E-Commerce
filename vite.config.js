5fee8fd1e4c945cbf4daa2ed0584df87e761b3a7
shoibe_sakib <shoibesakib81@gmail.com>
2025-12-10 19:26:23 +0600

    Fix GitHub Pages path

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  base: '/DoDo-E-Commerce/',  // ← IMPORTANT
 })
+
+
+
