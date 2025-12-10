8274c6dc5569252b3532151db791d67a78a8bb1e
shoibe_sakib <shoibesakib81@gmail.com>
2025-12-10 19:44:19 +0600

    d

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router'
 import Rootlayouts from "../layouts/Rootlayouts";
 import Home from "../pages/Home/Home/Home";
 import Aboutus from "../pages/aboutus/Aboutus";
@@ -7,12 +7,14 @@ import Aboutus from "../pages/aboutus/Aboutus";
 const Router = () => {
   return (
          <div className="font-urbanist max-w-11/12 mx-auto bg-gray-100">
-  <Routes>
+<HashRouter>
+         <Routes>
          <Route element={<Rootlayouts></Rootlayouts>}>
               <Route path="/" element={<Home></Home>}></Route>
               <Route path="/aboutus" element={<Aboutus></Aboutus>}></Route>
         </Route>
        </Routes>
+</HashRouter>
 
          </div>
      
